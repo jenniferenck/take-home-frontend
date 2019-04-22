548b05155eeb540c6306f0aee8c1c01b14fb5cee
Jennifer Enck <jenniferenck@mac.com>
2019-04-22 10:55:05 -0700

    added functionality to handle search and typing

--- src/SearchBar/index.js
+++ src/SearchBar/index.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
- 
-    class SearchBar extends Component {
-        render() {
-            return (
-                <div>
-                    <input placeholder="Search for a gif here!" type="text"/>
-                </div>
-            );
-        }
-    }
- 
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchTerm: '' };
+  }
+  handleSubmit = evt => {
+    evt.preventDefault();
+    this.props.handleSearch(this.state.searchTerm);
+    this.setState({ searchTerm: '' });
+  };
+
+  handleChange = evt => {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  };
+
+  render() {
+    const { handleReset } = this.props;
+    return (
+      <form onSumit={this.handleSubmit} id="search-form">
+        <input
+          placeholder="Search for a gif here!"
+          type="text"
+          name="searchTerm"
+          value={this.state.searchTerm}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit} id="search">
+          Search
+        </button>
+        <button onClick={handleReset} id="reset-search">
+          Reset
+        </button>
+      </form>
+    );
+  }
+}
+
 export default SearchBar;
