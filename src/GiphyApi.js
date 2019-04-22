c844c0450bf1aa75f9b4f8adf1973698605c7b36
Jennifer Enck <jenniferenck@mac.com>
2019-04-22 13:18:55 -0700

    added first axios request for 'trending'

--- src/GiphyApi.js
+++ src/GiphyApi.js
@@ -1,14 +1,18 @@
+import GIPHY_KEY from './env';
 const axios = require('axios');
-const BASE_URL = 'api.giphy.com';
+
+const BASE_URL = `https://api.giphy.com/v1/gifs/`;
 
 class GiphyApi {
-  // gifs should return gifs in the following format:
+  // results are up to 25 gifs with keys of data, meta, and pagination:
 
-  fetchTrendingGifs() {
+  static async fetchTrendingGifs() {
     // fetches currently trending gifs
+    const results = await axios.get(`${BASE_URL}trending?api_key=${GIPHY_KEY}`);
+    return results.data;
   }
 
-  fetchGifs(searchTerm) {
+  static async fetchGifs(searchTerm) {
     // fetches gifs based on searchTerm
   }
 }
