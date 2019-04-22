e0195606589030c2473e5d2b7681acb2545eab3a
Jennifer Enck <jenniferenck@mac.com>
2019-04-22 10:04:25 -0700

    added initial components

---
+++ src/Giph/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+ 
+    class Giph extends Component {
+        render() {
+            return (
+                <div>I'm a giph placeholder</div>
+            );
+        }
+    }
+ 
+export default Giph;
---
+++ src/GiphList/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Giph from '../Giph';
+ 
+    class GiphList extends Component {
+        render() {
+            return(
+                <Giph />
+            );
+        }
+    }
+ 
+export default GiphList;
