9939d6e1bad6d727a2c9648d640c29479e9edaa1
Jennifer Enck <jenniferenck@mac.com>
2019-04-22 11:10:50 -0700

    added structure for giphy api requests

---
+++ src/GiphyApi.js
@@ -0,0 +1,15 @@
+const BASE_URL = 'api.giphy.com';
+
+class GiphyApi {
+  // gifs should return gifs in the following format:
+
+  fetchTrendingGifs() {
+    // fetches currently trending gifs
+  }
+
+  fetchGifs(searchTerm) {
+    // fetches gifs based on searchTerm
+  }
+}
+
+export default GiphyApi;
