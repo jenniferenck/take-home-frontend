363d6711da2c150bd99d8a0af9773721d9b5c0be
Jennifer Enck <jenniferenck@mac.com>
2019-04-22 10:33:10 -0700

    added 'pure' component to monitor state change

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import './App.css';
 import GifList from '../GifList';
 import SearchBar from '../SearchBar';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -17,6 +17,12 @@ class App extends Component {
   // on initial page load, fetch trending Gifs
   componentDidMount() {
     // API request to giphy for most current gifs
+  }
+
+  fetchGifs(searchTerm) {
+    // API request for gifs that meet search term
+    // set state of activeSearch to true
+    // once results come in, setState of recentSearchGifs to equal results
   }
 
   render() {
