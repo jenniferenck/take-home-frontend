de9255fd242a92b3a7c9ccff2c35bff699e55b21
Jennifer Enck <jenniferenck@mac.com>
2019-04-22 10:20:20 -0700

    added initial app 'state'

--- src/App/index.js
+++ src/App/index.js
@@ -5,14 +5,31 @@ import GiphList from '../GiphList';
 import SearchBar from '../SearchBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trendingGifs: [],
+      recentSearchGifs: [],
+      activeSearch: false
+    };
+  }
+
+  // on initial page load, fetch trending Gifs
+  componentDidMount() {
+    // API request to giphy for most current gifs
+  }
+
   render() {
+    const { recentSearchGifs, trendingGifs } = this.state;
     return (
       <div className="App">
         <div className="App-header">
           <h2>Let's get Giphy</h2>
-          <SearchBar />
-          <GiphList />
         </div>
+        <SearchBar />
+        <GiphList
+          gifs={this.state.activeSearch ? recentSearchGifs : trendingGifs}
+        />
       </div>
     );
   }
