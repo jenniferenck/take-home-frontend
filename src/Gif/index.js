6e52a85a251e91fa1007168fa1a9487970ef0102
Jennifer Enck <jenniferenck@mac.com>
2019-04-23 14:36:04 -0700

    added favorite transition and fixed flashing

--- src/Gif/index.js
+++ src/Gif/index.js
@@ -4,21 +4,28 @@ import './Gif.css';
 class Gif extends Component {
   constructor(props) {
     super(props);
-    this.handleDoubleTap = this.handleDoubleTap.bind(this);
+    this.state = { favorited: false };
+    this.toggleFavorite = this.toggleFavorite.bind(this);
   }
 
-  handleDoubleTap(evt) {
-    evt.preventDefault();
-    console.log('double tapped!');
+  toggleFavorite(evt) {
+    // console.log('double tapped!');
+    this.setState(st => ({ favorited: !st.favorited }));
   }
 
   render() {
+    console.log(this.state.favorited);
     const { title, source, imageUrl, rating } = this.props;
     return (
       <div className="gif" onDoubleClickCapture={this.handleDoubleTap}>
         <img src={imageUrl} alt={title} />
         <div className="text-overlay">
-          <i className="far fa-heart" />
+          <i
+            onClick={this.toggleFavorite}
+            className={`far fa-heart ${
+              this.state.favorited ? 'favorited' : ''
+            }`}
+          />
           <div>
             <h4>Title:</h4>
             <p>{title.length ? title : 'Not available'}</p>
