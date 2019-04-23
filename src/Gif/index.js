c69d0bfae0964d6d0033b8648ca374497f437140
Jennifer Enck <jenniferenck@mac.com>
2019-04-23 10:59:08 -0700

    added hovering effect

--- src/Gif/index.js
+++ src/Gif/index.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import './Gif.css';
 
 class Gif extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hovering: false };
+    this.toggleHover = this.toggleHover.bind(this);
+  }
+
+  toggleHover() {
+    this.setState(st => ({
+      hovering: !st.hovering
+    }));
+  }
   render() {
     return (
       <div className="gif" onClick={() => this.props.handleClick(this.props)}>
-        <img src={this.props.imageUrl} alt={this.props.title} />
+        <img
+          onMouseEnter={this.toggleHover}
+          onMouseLeave={this.toggleHover}
+          className={this.state.hovering ? 'hovering' : ''}
+          src={this.props.imageUrl}
+          alt={this.props.title}
+        />
       </div>
     );
   }
