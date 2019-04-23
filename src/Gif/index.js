2874ae64f2ab75672c9e75ac5392b1a434e0b5e8
Jennifer Enck <jenniferenck@mac.com>
2019-04-23 12:37:33 -0700

    switched handling of hover events to css

--- src/Gif/index.js
+++ src/Gif/index.js
@@ -5,28 +5,40 @@ class Gif extends Component {
   constructor(props) {
     super(props);
     this.state = { hovering: false };
-    this.toggleHover = this.toggleHover.bind(this);
+    this.handletoggleHover = this.handletoggleHover.bind(this);
+    this.handleDoubleTap = this.handleDoubleTap.bind(this);
   }
 
-  toggleHover() {
+  handletoggleHover() {
     this.setState(st => ({
       hovering: !st.hovering
     }));
   }
+
+  handleDoubleTap(evt) {
+    evt.preventDefault();
+    console.log('double tapped!');
+  }
+
   render() {
+    const { title, source, imageUrl, rating } = this.props;
     return (
-      <div className="gif" onClick={() => this.props.handleClick(this.props)}>
-        <i
+      <div
+        className="gif"
+        onClick={() => this.props.handleClick(this.props)}
+        onDoubleClickCapture={this.handleDoubleTap}
+      >
+        {/* <i
           className={
             this.state.hovering ? 'far fa-heart' : 'far fa-heart-hidden'
           }
-        />
+        /> */}
         <img
-          onMouseEnter={this.toggleHover}
-          onMouseLeave={this.toggleHover}
+          // onMouseEnter={this.handletoggleHover}
+          // onMouseLeave={this.handletoggleHover}
           className={this.state.hovering ? 'hovering' : ''}
-          src={this.props.imageUrl}
-          alt={this.props.title}
+          src={imageUrl}
+          alt={title}
         />
       </div>
     );
