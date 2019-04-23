b6a59b1b4f33f3d4a34f1ddb6c958b297fafa320
Jennifer Enck <jenniferenck@mac.com>
2019-04-22 18:47:56 -0700

    added modal info

--- src/GiphModal/index.js
+++ src/GiphModal/index.js
@@ -3,10 +3,15 @@ import './GiphModal.css';
 
 class GiphModal extends Component {
   render() {
+    const { currentGif, handleClose } = this.props;
+    console.log(currentGif);
     return (
       <div className="modal">
-        placeholder for the giph info on click
-        <button onClick={this.props.handleClose}>close</button>
+        <h3>Title: {currentGif.title}</h3>
+        <h3>Rating: {currentGif.rating}</h3>
+        <h3>Source: {currentGif.source}</h3>
+
+        <button onClick={handleClose}>close</button>
       </div>
     );
   }
