8beeb2d76225874d2f44aab1b3c817fe7e590219
Jennifer Enck <jenniferenck@mac.com>
2019-04-24 10:00:53 -0700

    separated render views

--- src/GifList/index.js
+++ src/GifList/index.js
@@ -3,26 +3,50 @@ import Gif from '../Gif';
 import './GifList.css';
 
 class GifList extends Component {
+  displayFavorites = () => {
+    const { handleAddOrRemoveFav, favoritedGifs } = this.props;
+
+    for (let gif in favoritedGifs) {
+      return (
+        <Gif
+          favorited={favoritedGifs[gif.id] ? true : false}
+          key={gif.id}
+          id={gif.id}
+          title={gif.title}
+          imageUrl={gif.images.fixed_height.url}
+          source={gif.source}
+          rating={gif.rating}
+          handleAddOrRemoveFav={handleAddOrRemoveFav}
+        />
+      );
+    }
+  };
+
+  displayNormalView = () => {
+    const { handleAddOrRemoveFav, favoritedGifs, gifs } = this.props;
+    console.log('gifs:', gifs);
+    return gifs.length ? (
+      gifs.map(gif => (
+        <Gif
+          favorited={favoritedGifs[gif.id] ? true : false}
+          key={gif.id}
+          id={gif.id}
+          title={gif.title}
+          imageUrl={gif.images.fixed_height.url}
+          source={gif.source}
+          rating={gif.rating}
+          handleAddOrRemoveFav={handleAddOrRemoveFav}
+        />
+      ))
+    ) : (
+      <h2>GIFs Loading...</h2>
+    );
+  };
   render() {
-    const { gifs, handleAddOrRemoveFav, favoritedGifs } = this.props;
+    const { favoritesView } = this.props;
     return (
       <div className="gif-list-section">
-        {gifs.length ? (
-          gifs.map(gif => (
-            <Gif
-              favorited={favoritedGifs[gif.id] ? true : false}
-              key={gif.id}
-              id={gif.id}
-              title={gif.title}
-              imageUrl={gif.images.fixed_height.url}
-              source={gif.source}
-              rating={gif.rating}
-              handleAddOrRemoveFav={handleAddOrRemoveFav}
-            />
-          ))
-        ) : (
-          <h2>GIFs Loading...</h2>
-        )}
+        {favoritesView ? this.displayFavorites() : this.displayNormalView()}
       </div>
     );
   }
