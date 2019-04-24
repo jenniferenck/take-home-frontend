33b36871c02c4dd8f88bba792d7312e5e0d86af9
Jennifer Enck <jenniferenck@mac.com>
2019-04-24 11:47:28 -0700

    corrected looping of favorites for rendering view

--- src/GifList/index.js
+++ src/GifList/index.js
@@ -3,28 +3,30 @@ import Gif from '../Gif';
 import './GifList.css';
 
 class GifList extends Component {
+  // store favorites in arr to render each favorite
   displayFavorites = () => {
     const { handleAddOrRemoveFav, favoritedGifs } = this.props;
+    const favoritesArr = [];
 
-    for (let gif in favoritedGifs) {
-      return (
-        <Gif
-          favorited={favoritedGifs[gif.id] ? true : false}
-          key={gif.id}
-          id={gif.id}
-          title={gif.title}
-          imageUrl={gif.images.fixed_height.url}
-          source={gif.source}
-          rating={gif.rating}
-          handleAddOrRemoveFav={handleAddOrRemoveFav}
-        />
-      );
+    for (let fav in favoritedGifs) {
+      favoritesArr.push(favoritedGifs[fav]);
     }
+    return favoritesArr.map(gif => (
+      <Gif
+        favorited={favoritedGifs[gif.id] ? true : false}
+        key={gif.id}
+        id={gif.id}
+        title={gif.title}
+        imageUrl={gif.imageUrl}
+        source={gif.source}
+        rating={gif.rating}
+        handleAddOrRemoveFav={handleAddOrRemoveFav}
+      />
+    ));
   };
 
   displayNormalView = () => {
     const { handleAddOrRemoveFav, favoritedGifs, gifs } = this.props;
-    console.log('gifs:', gifs);
     return gifs.length ? (
       gifs.map(gif => (
         <Gif
@@ -44,6 +46,7 @@ class GifList extends Component {
   };
   render() {
     const { favoritesView } = this.props;
+    console.log('rendering view type:', favoritesView);
     return (
       <div className="gif-list-section">
         {favoritesView ? this.displayFavorites() : this.displayNormalView()}
